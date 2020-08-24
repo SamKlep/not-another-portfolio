a91181559ca9632f28002c92d29dcadbafd2291f
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-08-24 15:29:19 -0500

    About page content created.

--- src/components/AboutMain.js
+++ src/components/AboutMain.js
@@ -3,24 +3,25 @@ import React from "react";
 const AboutMain = () => {
   return (
     <div>
-      <section className='section is-large'>
-        <div className='container'>
-          <h1 className='title'>About</h1>
-          <h2 className='subtitle'>something else about me.</h2>
+      <section className='section is-large about-image'>
+        <div className='container  '>
+          <div className='container  '>
+            <h1 className='title'>About</h1>
+            <h2 className='subtitle'></h2>
 
-          <figure className='image is-128x128'>
-            <img
-              className='is-rounded'
-              src='/assets/images/sam_portfolio_face.png'
-            />
-          </figure>
-          <p>
-            Nam quis tellus risus. Fusce id leo id ex semper laoreet. Cras quis
-            pellentesque nunc. Donec at sem sed risus congue consequat id eu
-            leo. Sed sed aliquet eros, et tempus diam. Phasellus ac ligula at
-            eros commodo ullamcorper id rutrum purus. Curabitur bibendum velit
-            ut rhoncus bibendum. Vestibulum ac ligula non ex aliquam porta.
-          </p>
+            <figure className='image is-128x128 about-image'>
+              <img
+                className='is-rounded'
+                src='/assets/images/sam_portfolio_face.png'
+              />
+            </figure>
+            <p>
+              I have a passion for all things tech and have been intrigued by it
+              all my life-- especially the blockchain and cryptocurrency. I
+              primarily work in Javascript, NodeJS and React but have recently
+              started learning React VR and Django.
+            </p>
+          </div>
         </div>
       </section>
     </div>
