b6f898f9ecad94237a41fb16b38e6a4e8bedc8b5
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-08-25 14:54:52 -0500

    Updaed logo.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -32,21 +32,41 @@ export const Navigation = () => {
 
         <div id='navbarBasicExample' className='navbar-menu'>
           <div className='navbar-start'>
-            <a className='navbar-item'>Home</a>
-
-            <a className='navbar-item'>About</a>
-
-            <div className='navbar-item has-dropdown is-hoverable'>
-              <a className='navbar-link'>More</a>
-
-              <div className='navbar-dropdown'>
-                <a className='navbar-item'>About</a>
-                <a className='navbar-item'>Jobs</a>
-                <a className='navbar-item'>Contact</a>
-                <hr className='navbar-divider' />
-                <a className='navbar-item'>Report an issue</a>
-              </div>
-            </div>
+            <Link
+              to='/'
+              className='navbar-item navbar-menu is-active'
+              style={{ fontSize: "1.3em" }}
+            >
+              Home
+            </Link>
+            <Link
+              to='/about'
+              className='navbar-item'
+              style={{ fontSize: "1.3em" }}
+            >
+              About
+            </Link>
+            <Link
+              to='/skills'
+              className='navbar-item'
+              style={{ fontSize: "1.3em" }}
+            >
+              Skills
+            </Link>
+            <Link
+              to='/projects'
+              className='navbar-item'
+              style={{ fontSize: "1.5em" }}
+            >
+              Projects
+            </Link>
+            <Link
+              to='/contact'
+              className='navbar-item'
+              style={{ fontSize: "1.3em" }}
+            >
+              Contact
+            </Link>
           </div>
 
           <div className='navbar-end'>
