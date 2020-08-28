407139f6d73435a5d1332800f1a5a7653b4b61fa
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-08-28 13:42:52 -0500

    about content created.

--- src/components/AboutMain.js
+++ src/components/AboutMain.js
@@ -17,14 +17,21 @@ const AboutMain = () => {
               <br />
               <br />
               <p className='is-size-5'>
-                I have experience working as a freelance developer and manage a
-                few websites for clients. I come from a design background and
-                utilize that skill in to my designs. I have a passion for all
-                things tech and have been intrigued by it all my life--
-                especially the blockchain and cryptocurrency. I primarily work
-                in Javascript, NodeJS, Express, MongoDB and React; but have
-                recently started learning React VR, Django and IoT devices
-                (Raspberry Pi, Arduino, ESP8266).
+                I have experience working as a freelance web developer and
+                manage a few websites for clients. I come from a graphic design
+                & illustration background—- so naturally I incorporate those
+                skills in to my web development process. I have a passion for
+                all things tech and have been intrigued by it all my life. I
+                primarily work with the MERN stack but have lately been trying
+                out new languages and frameworks.
+                <br />
+                <br />
+                Aside from being a web developer, I like to enjoy the great
+                outdoors as much as possible. I enjoy gardening, bird-watching,
+                and feeding squirrels. I’m an avid science-fiction reader and a
+                long-time Spider-Man comic-book collector. In spare time, I like
+                to paint and take pictures of nature. I also have three lovely
+                felines and one loyal canine companion.
               </p>
               <br />
               <a
